19029b7d7355411e49caab043f3ad872950e89ea
Alexey Voropay <monkena@yandex.ru>
2020-05-05 12:17:57 +0300

    +1

--- test2.js
+++ test2.js
@@ -1,9 +1,13 @@
 const str = 'nradeeepp';
-
+for (let i = 0; i < str.lentgth; i++) {
+  console.log(1);
+}
+/*
 // eslint-disable-next-line no-plusplus
 for (let i = 0; i < str.lentgth; i++) {
   // eslint-disable-next-line eqeqeq
   if (str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i)))
-   global.console.log(str.charAt(i));
+   console.log(str.charAt(i));
   break;
 }
+*/
